0435881a60f0f5e1140301043988dddc5b58bcad
TheNikhil13 <109781732+TheNikhil13@users.noreply.github.com>
2022-09-03 11:29:49 +0530

    First commit
    First commit

---
+++ test1.php
@@ -0,0 +1,14 @@
+<?php
+$n=array('n','a','g',"s",'e','n');
+$RollNo=array(1,0,0,1);
+sort($n, SORT_STRING);
+sort($RollNo, SORT_NUMERIC);
+foreach ($n as $row)
+echo $row;
+foreach ($RollNo as $r)
+echo $r;
+?>
+aegnns0011
+sngaen
+
+
